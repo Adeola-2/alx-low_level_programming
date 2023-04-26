115a87c26ac6d004a1ae5754a3067ef2a21e335a
Adeola-2 <rebeccaadedeji2@gmail.com>
2023-04-26 02:57:19 -0700

    my news

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _putchar(int c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
+
