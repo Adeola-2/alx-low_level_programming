33b58ae2e54f1212659355bfe077804e9dc86997
Adeola-2 <rebeccaadedeji2@gmail.com>
2023-04-26 18:37:40 -0700

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -14,20 +14,3 @@ int _islower(int c)
 		return (0);
 }
 
-
-Task 4
-
-#include "main.h"
-/**
- * _isalpha - Checks for alphabetic order
- *
- * @c: alpabet to check for
- *
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-
-}
-
